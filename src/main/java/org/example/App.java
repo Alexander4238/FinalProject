730d8a9897a435d96fee2beb83069bfa93df29ab
alex_stivi <alexstividor@gmail.com>
2024-10-24 19:46:27 +0200

    amend gitignore

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class App {
+\ublic class App {
     private static boolean uiExit = true;
 
     private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));

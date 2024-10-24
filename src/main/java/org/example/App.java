72b841f68b4bbccc0178fe8d1172b60cb7c7f0e5
alex_stivi <alexstividor@gmail.com>
2024-10-24 09:50:19 +0200

    add ui test logic

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,31 @@
 package org.example;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class App {
+    private static boolean uiExit = true;
+
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) {
+        String choice;
+        while (uiExit) {
+            System.out.println("This is a sort application");
+            System.out.println("Select data source to sort:");
+            System.out.println("f - from file");
+            System.out.println("r - random data");
+            System.out.println("c - custom data");
+            System.out.println("e - exit");
+            try {
+                choice = reader.readLine();
+                if (choice.equalsIgnoreCase("Y")) {
+                    uiExit = false;
+                }
+            } catch (IOException e) {
+                System.out.println("pls enter correct letter");
+            }
+        }
     }
 }
